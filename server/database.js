0a493db54a14ebdc525159885dbc3d4740881642
Veis1337 <117036853+Veis1337@users.noreply.github.com>
2023-06-05 15:37:54 -0400

    redeploy

--- server/database.js
+++ server/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const devConfig = {
   user: process.env.DB_USER,
-  password: 'water111',
+  password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
