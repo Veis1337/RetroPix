3157fe2fa43de7f9c0676ceb50b8cf3368f7f64e
Veis1337 <117036853+Veis1337@users.noreply.github.com>
2023-06-06 16:18:38 -0400

    redeploy

--- server/database.js
+++ server/database.js
@@ -10,13 +10,21 @@ const devConfig = {
 };
 
 // Deployment
-const proConfig = {
-  connectionString: process.env.DATABASE_URL,
-  dialect: 'postgres',
-};
+function getProdConfig() {
+  // we have to parse url string that heroku updates
+  const dbUrl = new URL(process.env.DATABASE_URL); // URL is a class is JS for this
+  return {
+    user: dbUrl.username,
+    password: dbUrl.password,
+    host: dbUrl.host,
+    database: dbUrl.pathname.replace("/", ""), // it's the /do8.... we have to remove the slash though
+    dialect: 'postgres',
+  };
+}
+
 
 const sequelize = new Sequelize(
-  process.env.NODE_ENV === 'production' ? proConfig : devConfig
+  process.env.NODE_ENV === 'production' ? getProdConfig() : devConfig
 );
 
 module.exports = sequelize;
